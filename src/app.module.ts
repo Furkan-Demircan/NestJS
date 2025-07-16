db5d3b8a418f590f7d3d37c3de2b0c87d0439346
Furkan Demircan <goooglenudle@gmail.com>
2025-07-16 16:08:27 +0300

    feat: configure TypeORM with PostgreSQL and enhance error handling on bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TodoModule } from './todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Todo } from './todo/entity/todo.entity';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nest',
+      password: 'nest',
+      database: 'nest',
+      entities: [Todo],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
